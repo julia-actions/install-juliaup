09e782bdc56b8debcb0aef2f5b80b85640f936f4
Dilum Aluthge <dilum@aluthge.com>
2024-03-25 11:23:57 -0400

    Document that the `juliaup-version` input is not part of the public API

--- src/inputs.ts
+++ src/inputs.ts
@@ -2,27 +2,29 @@
 import * as core from '@actions/core'
 
 export function get_julia_version_input() {
-    const original_version_input = core.getInput('julia-version')
+    const input_name = 'julia-version'
+    const original_version_input = core.getInput(input_name)
     const version = original_version_input.trim()
 
     if (version != original_version_input) {
-        throw new Error(`Version input is not allowed to begin or end with whitespace: ${original_version_input}`)
+        throw new Error(`The ${input_name} input is not allowed to begin or end with whitespace: ${original_version_input}`)
     }
     if (!isNonEmptyString(version)) {
-        throw new Error(`You must specify a value for the version input`)
+        throw new Error(`You must specify a value for the ${input_name} input`)
     }
     return version
 }
 
 export function get_juliaup_version_input() {
-    const original_version_input = core.getInput('juliaup-version')
+    const input_name = 'juliaup-version'
+    const original_version_input = core.getInput(input_name)
     const version = original_version_input.trim()
 
     if (version != original_version_input) {
-        throw new Error(`Version input is not allowed to begin or end with whitespace: ${original_version_input}`)
+        throw new Error(`The ${input_name} input is not allowed to begin or end with whitespace: ${original_version_input}`)
     }
     if (!isNonEmptyString(version)) {
-        throw new Error(`You must specify a value for the version input`)
+        throw new Error(`You must specify a value for the ${input_name} input`)
     }
     return version
 }
