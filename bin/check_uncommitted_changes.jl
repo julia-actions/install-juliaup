a53e1e55cfbc4af1368ce7743d34c3c9595f2a05
Dilum Aluthge <dilum@aluthge.com>
2024-03-25 11:35:10 -0400

    CI: fix a typo in a CI script

--- bin/check_uncommitted_changes.jl
+++ bin/check_uncommitted_changes.jl
@@ -8,7 +8,7 @@ wait(proc)
 
 if !success(proc)
     msg = "##[error] found changed files after build. " *
-           "Please run `make pack`` and check in all changes."
+           "Please run `make pack` and check in all changes."
     println(stderr, msg)
     exit(1)
 end
