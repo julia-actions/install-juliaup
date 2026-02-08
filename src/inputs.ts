eb9480bb2cd7fd8dc837503e83c81d62336a26cc
Dilum Aluthge <dilum@aluthge.com>
2026-02-07 23:45:29 -0500

    Automatically use the latest v1 version of Juliaup

--- src/inputs.ts
+++ src/inputs.ts
@@ -29,6 +29,20 @@ export function get_juliaup_version_input() {
     return trimmed_input
 }
 
+export function get_github_token_input() {
+    const input_name = 'token'
+    const original_input = core.getInput(input_name)
+    const trimmed_input = original_input.trim()
+
+    if (trimmed_input != original_input) {
+        throw new Error(`The ${input_name} input is not allowed to begin or end with whitespace: ${original_input}`)
+    }
+    if (!isNonEmptyString(trimmed_input)) {
+        throw new Error(`You trimmed_input specify a value for the ${input_name} input`)
+    }
+    return trimmed_input
+}
+
 export function isNonEmptyString(str: string) {
     const result = str && str.length > 0;
     return result
