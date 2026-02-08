69d7b1e746b7e82e70a2ea16c33b56579dd619c6
Dilum Aluthge <dilum@aluthge.com>
2026-02-07 23:55:58 -0500

    NFC: Rename some internal variables

--- src/inputs.ts
+++ src/inputs.ts
@@ -3,30 +3,30 @@ import * as core from '@actions/core'
 
 export function get_juliaup_channel_input() {
     const input_name = 'channel'
-    const original_version_input = core.getInput(input_name)
-    const version = original_version_input.trim()
+    const original_input = core.getInput(input_name)
+    const trimmed_input = original_input.trim()
 
-    if (version != original_version_input) {
-        throw new Error(`The ${input_name} input is not allowed to begin or end with whitespace: ${original_version_input}`)
+    if (trimmed_input != original_input) {
+        throw new Error(`The ${input_name} input is not allowed to begin or end with whitespace: ${original_input}`)
     }
-    if (!isNonEmptyString(version)) {
+    if (!isNonEmptyString(trimmed_input)) {
         throw new Error(`You must specify a value for the ${input_name} input`)
     }
-    return version
+    return trimmed_input
 }
 
 export function get_juliaup_version_input() {
     const input_name = 'internal-juliaup-version'
-    const original_version_input = core.getInput(input_name)
-    const version = original_version_input.trim()
+    const original_input = core.getInput(input_name)
+    const trimmed_input = original_input.trim()
 
-    if (version != original_version_input) {
-        throw new Error(`The ${input_name} input is not allowed to begin or end with whitespace: ${original_version_input}`)
+    if (trimmed_input != original_input) {
+        throw new Error(`The ${input_name} input is not allowed to begin or end with whitespace: ${original_input}`)
     }
-    if (!isNonEmptyString(version)) {
+    if (!isNonEmptyString(trimmed_input)) {
         throw new Error(`You must specify a value for the ${input_name} input`)
     }
-    return version
+    return trimmed_input
 }
 
 export function isNonEmptyString(str: string) {
